50ed1e4589b152a0932345d6de4540291202ace0
blucheez <nathanalam99@gmail.com>
2017-05-21 09:42:22 -0400

    debug

--- control.js
+++ control.js
@@ -13,10 +13,8 @@ var client = new Twitter({
 
 function search(term) {
     client.get('search/tweets', {q: term}, function(error, tweets, response) {
-       console.log(tweets);
        return tweets;
     });
-    return "something went wrong";
 }
 
 app.use("/ubi",  express.static(__dirname + '/ubi'));
