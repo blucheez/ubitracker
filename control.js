d4116f5344a28b5e9963cebc19a0b231df8634c6
blucheez <nathanalam99@gmail.com>
2017-05-20 21:46:51 -0400

    asdf

--- control.js
+++ control.js
@@ -12,6 +12,22 @@ app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname + '/ubi/index.html'));
 });
 
+app.get('/index.html', function(req, res) {
+    res.sendFile(path.join(__dirname + '/ubi/index.html'));
+});
+
+app.get('/listing.html', function(req, res) {
+    res.sendFile(path.join(__dirname + '/ubi/listing.html'));
+});
+
+app.get('/about.html', function(req, res) {
+    res.sendFile(path.join(__dirname + '/ubi/about.html'));
+});
+
+app.get('/twitter.html', function(req, res) {
+    res.sendFile(path.join(__dirname + '/ubi/twitter.html'));
+});
+
 var listener = app.listen(app.get('port'), function() {
   console.log( listener.address().port );
 });
