83df82a4cf0a8e8fe454e9dc8a2ea9aa9f59ed56
blucheez <nathanalam99@gmail.com>
2017-05-24 21:43:18 -0400

    added ui to twitter

--- ubi/twitterscript.js
+++ ubi/twitterscript.js
@@ -3,7 +3,7 @@ $.get("https://ubitracker.herokuapp.com/twitsearch", function(data) {
     var stats = data.statuses;
     for(var i = 0; i < stats.length; i++) {
         var name = stats[i].user.name;
-        var text = stats[i].user.text;
+        var text = stats[i].text;
         $("#frame").append("<div id = 'pane" + i + "' class='panel panel-default'></div>");
         
         $("#pane" + i).append("<div id='title" + i + "' class='panel-heading'></div>");
